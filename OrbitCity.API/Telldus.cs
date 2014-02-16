0939e0cb87fc273dbf47bf516f2426bd67c15ba3
Frida Nellros <frida.nellros@gmail.com>
2014-02-16 21:03:30 +0100

    * first draft text gui

---
+++ OrbitCity.API/Telldus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace OrbitCity.API.Client
+{
+    public class Telldus
+    {
+        private string Url;
+        private HttpClient Hc;
+
+        public Telldus(string url)
+        {
+            this.Url = url;
+            this.Hc = new HttpClient();
+            Hc.BaseAddress = new Uri(url);
+            Hc.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public T Get<T>(string path)
+        {
+            HttpResponseMessage rsp = Hc.GetAsync(path).Result;
+            rsp.EnsureSuccessStatusCode(); //TODO catch 
+            return rsp.Content.ReadAsAsync<T>().Result;
+        }
+
+        public T Post<T,D>(string path, D data)
+        {
+            HttpResponseMessage rsp = Hc.PostAsJsonAsync<D>(path, data).Result;
+            rsp.EnsureSuccessStatusCode(); //TODO catch 
+            return rsp.Content.ReadAsAsync<T>().Result;
+        }
+    }
+}
---
+++ OrbitCity.GUI/Controllers/TdDeviceController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrbitCity.API.Client;
+using OrbitCity.API.DTO;
+
+namespace OrbitCity.GUI.Controllers
+{
+    public class TdDeviceController : Controller
+    {
+        string server;
+        private Telldus Td;
+
+        public TdDeviceController()
+        {
+            server = "http://192.168.1.94:4567";
+            Td = new Telldus(server);
+        }
+
+        public ActionResult Index()
+        {
+            List<TdDevice> TdList = Td.Get<List<TdDevice>>("device/all");
+            return View( TdList );
+        }
+
+
+        public ActionResult On(int Id)
+        {
+            Td.Get<dynamic>("device/" + Id + "/switch/on");
+            return new EmptyResult();
+        }
+        // /tddevice/off/:id
+        public ActionResult Off(int Id)
+        {
+            Td.Get<dynamic>("device/" + Id + "/switch/off");
+            return new EmptyResult();
+        }
+        // /tddevice/dim/:id?Lvl=20
+        public ActionResult Dim(int Id, int Lvl)
+        {
+            Td.Get<dynamic>("device/" + Id + "/dim/" + Lvl);
+            return new EmptyResult();
+        }
+    }
+}
